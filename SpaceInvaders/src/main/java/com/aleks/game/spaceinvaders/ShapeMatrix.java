fb9b0ea076463e18c31f6de3810026447b7661fb
Infinity2010 <arbiter2008@ya.ru>
2019-04-16 22:07:33 +0300

    Матрицы состояний игровых объектов

--- SpaceInvaders/src/main/java/com/aleks/game/spaceinvaders/ShapeMatrix.java
+++ SpaceInvaders/src/main/java/com/aleks/game/spaceinvaders/ShapeMatrix.java
@@ -1,11 +1,6 @@
 package com.aleks.game.spaceinvaders;
 
 public class ShapeMatrix {
-  public static final int[][] ENEMY = new int[][]{
-	  {7, 0, 7},
-	  {7, 7, 7},
-	  {0, 7, 0}
-  };
 
   public static final int[][] PLAYER = new int[][]{
 	  {0, 0, 6, 0, 0},
@@ -28,6 +23,12 @@ public class ShapeMatrix {
   public static final int[][] BULLET = new int[][]{
 	  {2},
 	  {2}
+  };
+
+  public static final int[][] ENEMY = new int[][]{
+	  {7, 0, 7},
+	  {7, 7, 7},
+	  {0, 7, 0}
   };
 
   public static final int[][] KILL_ENEMY_ANIMATION_FIRST = new int[][]{
